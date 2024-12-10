4b60978a6615746c2dc3f8829c8b482b678305d5
nishatislam04 <nishatislam3108@gmail.com>
2024-12-10 23:56:12 +0600

    Add: header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+import Nav from "./Nav";
+import { Button } from "./ui/button";
+
 export default function Header() {
-	return <p>header</p>;
+	return (
+		<header className="py-8 text-white xl:py-2 ">
+			<div className="container flex items-center justify-between mx-auto">
+				<Link href="/">
+					<h1 className="text-4xl font-semibold">
+						Luke <span className="text-accent">.</span>
+					</h1>
+				</Link>
+				<div className="items-center hidden gap-8 xl:flex">
+					<Nav />
+					<Link href="/contact">
+						<Button>Hire me</Button>
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
