00aac03a40c772fded026ea2ab3d4779ec9860cc
nishatislam04 <nishatislam3108@gmail.com>
2024-12-15 23:12:45 +0600

    fix: font not working

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ import StairTransition from "@/components/StairTransition";
 const jetbrainsMono = JetBrains_Mono({
 	subsets: ["latin"],
 	weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-	variable: "--forn-jetbrainsMono",
+	variable: "--font-jetbrainsMono",
 });
 
 export const metadata = {
