d36fa4724b65ec00539ae2bb55abe52749899702
nishatislam04 <nishatislam3108@gmail.com>
2024-12-24 02:36:42 +0600

    mobile:home:img position fix

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -18,7 +18,7 @@ export default function Photo() {
 						opacity: 1,
 						transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
 					}}
-					className="w-[250px] h-[250px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
+					className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
 					<Image
 						src="/assets/photo.png"
 						priority
